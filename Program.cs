d8396072c65128009b837be094b41e2ab2454e05
Your Name <nguy1601@usherbrooke.ca>
2025-06-26 15:40:11 -0400

    config settings files for ef migration and inject DbContext

--- Program.cs
+++ Program.cs
@@ -1,15 +1,22 @@
+﻿
+using Balance_API.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Balance_API
 {
     public class Program
     {
+
+
         public static void Main(string[] args)
         {
+
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
 
             builder.Services.AddControllers();
+            builder.Services.AddDbContext<BalanceDbContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
@@ -22,6 +29,12 @@ namespace Balance_API
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
+            using (var scope = app.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<BalanceDbContext>();
+                Console.WriteLine(db.Database.GetDbConnection().ConnectionString);
+                Console.WriteLine(db.Database.CanConnect() ? " Connexion OK" : "Connexion échouée");
+            }
 
             app.UseHttpsRedirection();
 
@@ -29,6 +42,13 @@ namespace Balance_API
 
 
             app.MapControllers();
+            using (var scope = app.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<BalanceDbContext>();
+                Console.WriteLine("Chaîne : " + db.Database.GetDbConnection().ConnectionString);
+                Console.WriteLine(db.Database.CanConnect() ? "✔ Connexion OK" : "❌ Connexion échouée");
+            }
+
 
             app.Run();
         }
